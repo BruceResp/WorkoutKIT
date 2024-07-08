c19ec26d115f044b430581812154abe4ea3ccce0
Bruce Ding <15158939322@163.com>
2024-07-08 14:00:58 +0800

    准备重构当前代码，先保存

--- STM32F103C8Tx_VSCODE_IDE/application/core/system/system.c
+++ STM32F103C8Tx_VSCODE_IDE/application/core/system/system.c
@@ -29,6 +29,15 @@ void System_Init(void){
     GUI_CLEAR_SCREEN();
     GUI_Shift_Menu( SystemCtrl.currentPage,SystemCtrl.targetPage);
 };
+
+/*----------------------------------------------------------------------------------------- 
+*函数名称:'System_MenuselectPage_Init' 
+*函数功能:'菜单选择界面 初始化' 
+*参    数:'' 
+*返 回 值:'' 
+*说    明: '在结束主菜单工作以后调用，初始化 动画的坐标等信息' 
+*作    者: Danny 
+*----------------------------------------------------------------------------------------*/ 
 void System_MenuselectPage_Init(void){
 
     SystemCtrl.Sys_InverseBox.current_x = 0;
@@ -42,39 +51,44 @@ void System_MenuselectPage_Init(void){
 	GUI_CLEAR_SCREEN();
 
 }
+
 /*----------------------------------------------------------------------------------------- 
 *函数名称:'System_GUIPagepointer_Sub' 
 *函数功能:'' 
 *参    数:'' 
 *返 回 值:'' 
-*说    明: '' 
+*说    明: '供外部调用' 
 *作    者: Danny 
 *----------------------------------------------------------------------------------------*/ 
 void System_GUIPagepointer_Sub(void){
     SystemCtrl.GUIpagePoint--;
 }
+
 /*----------------------------------------------------------------------------------------- 
 *函数名称:'System_GUIPagepointer_Add' 
 *函数功能:'' 
 *参    数:'' 
 *返 回 值:'' 
-*说    明: '' 
+*说    明: '供外部调用' 
 *作    者: Danny 
 *----------------------------------------------------------------------------------------*/ 
 void System_GUIPagepointer_Add(void){
     SystemCtrl.GUIpagePoint++;
 }
+
 /*----------------------------------------------------------------------------------------- 
 *函数名称:'System_Turn_Page' 
 *函数功能:'' 
 *参    数:'' 
 *返 回 值:'' 
-*说    明: '当set 释放以后，设置该位，并在判断完以后，调用System_Clear_turnPage' 
+*说    明: '当set 释放以后，设置该位，并在判断完以后，调用System_Clear_turnPage
+            供外部调用' 
 *作    者: Danny 
 *----------------------------------------------------------------------------------------*/ 
 void System_Turn_Page(void){
     SystemCtrl.is_turn_Page++;
 }
+
 /*----------------------------------------------------------------------------------------- 
 *函数名称:'System_Clear_turnPage' 
 *函数功能:'' 
@@ -115,70 +129,80 @@ void System_MainPage_Operation(void){
     }
 }
 
-void System_TrainMenuSelPage_Operation(void){
-    //Sytem_Load_Menu();
-    //uint8_t menuNum = FLASH_Read_MenuNum();
-    uint8_t menuNum = 8; //单纯的个数，从1开始
+/*----------------------------------------------------------------------------------------- 
+*函数名称:'System_Menu_shift_OP' 
+*函数功能:'菜单 移动 操作' 
+*参    数:'' 
+*返 回 值:'' 
+*说    明: '' 
+*作    者: Danny 
+*----------------------------------------------------------------------------------------*/ 
+void System_Menu_shift_OP(SystemCtrl_t* SystemCtrlPoint,uint8_t menuNum){
     static int8_t oldGUIpagePoint; 
-
-    if (menuNum == 0)
-    {
+    if (menuNum == 0){
         return;
     }
-    /*              update SystemCtrl.MenuPoint              */
-    if (SystemCtrl.GUIpagePoint - oldGUIpagePoint == 1 && (menuNum > 4))
-    {
+
+    if (SystemCtrlPoint->GUIpagePoint - oldGUIpagePoint == 1 && (menuNum > 4)){
         
-        SystemCtrl.MenuPoint++;
-        if (SystemCtrl.MenuPoint >= menuNum)
+        SystemCtrlPoint->MenuPoint++;
+        if (SystemCtrlPoint->MenuPoint >= menuNum)
         {
-            SystemCtrl.MenuPoint = menuNum - 1;       //如果超过菜单尾部  菜单尾部是menuNum - 1
+            SystemCtrlPoint->MenuPoint = menuNum - 1;                                               //如果超过菜单尾部  菜单尾部是menuNum - 1
         }
         if (oldGUIpagePoint >= GUI_ROWS_THIRD){
-            if (SystemCtrl.MenuPoint == (menuNum - 1))
+            if (SystemCtrlPoint->MenuPoint == (menuNum - 1))
             {
-                SystemCtrl.GUIpagePoint = GUI_ROWS_UP_LIMIT;
+                SystemCtrlPoint->GUIpagePoint = GUI_ROWS_UP_LIMIT;
             }       
         }
-        if ((oldGUIpagePoint == GUI_ROWS_THIRD) && (SystemCtrl.MenuPoint <= (menuNum - 2)))  //menupoint是指向下一条
+        if ((oldGUIpagePoint == GUI_ROWS_THIRD) && (SystemCtrlPoint->MenuPoint <= (menuNum - 2)))  //menupoint是指向下一条
         {
             Sys_Rowitem.target_y -= 16;
-            SystemCtrl.GUIpagePoint = GUI_ROWS_THIRD;
+            SystemCtrlPoint->GUIpagePoint = GUI_ROWS_THIRD;
         }
         
-    }else if (SystemCtrl.GUIpagePoint - oldGUIpagePoint == 1 && (menuNum <= 4))
+    }else if (SystemCtrlPoint->GUIpagePoint - oldGUIpagePoint == 1 && (menuNum <= 4))
     {
-        SystemCtrl.MenuPoint++;
-        if (SystemCtrl.MenuPoint >= menuNum)
+        SystemCtrlPoint->MenuPoint++;
+        if (SystemCtrlPoint->MenuPoint >= menuNum)
         {
-            SystemCtrl.GUIpagePoint = (menuNum - 1) ;
-            SystemCtrl.MenuPoint = (menuNum - 1) ;
+            SystemCtrlPoint->GUIpagePoint = (menuNum - 1) ;
+            SystemCtrlPoint->MenuPoint = (menuNum - 1) ;
         }
         
-    }else if (SystemCtrl.GUIpagePoint-oldGUIpagePoint == -1)
+    }else if (SystemCtrlPoint->GUIpagePoint-oldGUIpagePoint == -1)
     {
-        SystemCtrl.MenuPoint--;
-        if (SystemCtrl.MenuPoint < GUI_ROWS_DOWN_LIMIT){
-            SystemCtrl.MenuPoint = GUI_ROWS_DOWN_LIMIT;
+        SystemCtrlPoint->MenuPoint--;
+        if (SystemCtrlPoint->MenuPoint < GUI_ROWS_DOWN_LIMIT){
+            SystemCtrlPoint->MenuPoint = GUI_ROWS_DOWN_LIMIT;
         }
-        if (SystemCtrl.GUIpagePoint < GUI_ROWS_DOWN_LIMIT ){
-            SystemCtrl.GUIpagePoint = GUI_ROWS_DOWN_LIMIT;
+        if (SystemCtrlPoint->GUIpagePoint < GUI_ROWS_DOWN_LIMIT ){
+            SystemCtrlPoint->GUIpagePoint = GUI_ROWS_DOWN_LIMIT;
         }
-        if (SystemCtrl.GUIpagePoint == GUI_ROWS_DOWN_LIMIT && (SystemCtrl.MenuPoint > 0))
+        if (SystemCtrlPoint->GUIpagePoint == GUI_ROWS_DOWN_LIMIT && (SystemCtrlPoint->MenuPoint > 0))
         {
             Sys_Rowitem.target_y += 16;
         }
     }
-    /*              update SystemCtrl.MenuPoint              */
 
-    /*              SystemCtrl.MenuPoint under ctrol         */ 
-    /*              SystemCtrl.MenuPoint under ctrol         */
-    /*                  control inverse box                  */
-    
+    oldGUIpagePoint = SystemCtrlPoint->GUIpagePoint;
+}
 
-    
-    
-    oldGUIpagePoint = SystemCtrl.GUIpagePoint;
+/*----------------------------------------------------------------------------------------- 
+*函数名称:'System_TrainMenuSelPage_Operation' 
+*函数功能:'系统 训练菜单选择页面 操作' 
+*参    数:'' 
+*返 回 值:'' 
+*说    明: '' 
+*作    者: Danny 
+*----------------------------------------------------------------------------------------*/ 
+void System_TrainMenuSelPage_Operation(void){
+    //Sytem_Load_Menu();
+    //uint8_t menuNum = FLASH_Read_MenuNum();
+    uint8_t menuNum = 8;                                 //单纯的个数，从1开始
+
+    System_Menu_shift_OP(&SystemCtrl,menuNum);           //直接传是作为变量传递的，是一个副本
     
     if(SystemCtrl.is_turn_Page == 1){
         SystemCtrl.targetPage = SystemCtrl.currentPage +1;
